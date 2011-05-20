0e6b4775d268e309bb3bd9fa21c35c152acb830d
T.J. Telan <t.telan@gmail.com>
2011-05-20 02:19:03 -0700

    Adding prototypes for cgi gpio toggles

--- web/cgi.h
+++ web/cgi.h
@@ -7,5 +7,14 @@
 #ifndef _cgi_h_
 #define _cgi_h_
 
+int cgi_toggle_gpio1(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio2(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio3(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio4(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio5(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio6(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio7(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio8(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_gpio9(HTTPD_SESSION_STRUCT *session);
 
 #endif
