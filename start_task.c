a945216dcf8f5a49e5c0d319d512e0f6843af1d2
T.J. Telan <t.telan@gmail.com>
2011-05-20 02:23:31 -0700

    Formatting.

--- start_task.c
+++ start_task.c
@@ -13,39 +13,39 @@
 ----------------------------------------------------------------------------*/
 
 void start_task(uint_32 initial_data){
-   printf("Starting Start Task\n"); 
+    printf("Starting Start Task\n"); 
    
     FILE_PTR output_port;
 
     const uint_32 output_set[] = {
-//        GPIO1 | GPIO_PIN_STATUS_1,
-//        GPIO2 | GPIO_PIN_STATUS_1,
-//        GPIO3 | GPIO_PIN_STATUS_1,
-//        GPIO4 | GPIO_PIN_STATUS_1,
-//        GPIO5 | GPIO_PIN_STATUS_1,
-//        GPIO6 | GPIO_PIN_STATUS_1,
-//        GPIO7 | GPIO_PIN_STATUS_1,
-//        GPIO8 | GPIO_PIN_STATUS_1,
-//        GPIO9 | GPIO_PIN_STATUS_1,
+        GPIO1 | GPIO_PIN_STATUS_1,
+        GPIO2 | GPIO_PIN_STATUS_1,
+        GPIO3 | GPIO_PIN_STATUS_1,
+        GPIO4 | GPIO_PIN_STATUS_1,
+        GPIO5 | GPIO_PIN_STATUS_1,
+        GPIO6 | GPIO_PIN_STATUS_1,
+        GPIO7 | GPIO_PIN_STATUS_1,
+        GPIO8 | GPIO_PIN_STATUS_1,
+        GPIO9 | GPIO_PIN_STATUS_1,
+      
         BSP_LED1 | GPIO_PIN_STATUS_0,
         BSP_LED2 | GPIO_PIN_STATUS_0,
         BSP_LED3 | GPIO_PIN_STATUS_0,
         BSP_LED4 | GPIO_PIN_STATUS_0,
         GPIO_LIST_END
-  };
+    };
   
-  // Set GPIO array as outputs
-  output_port = fopen("gpio:write", (char_ptr) &output_set);
+    // Set GPIO array as outputs
+    output_port = fopen("gpio:write", (char_ptr) &output_set);
   
   
-  if (output_port){
+    // Test GPIO by turning on all ports
+    if (output_port){
         ioctl(output_port, GPIO_IOCTL_WRITE_LOG1, &output_port);
-
-  }
+    }
   
     /* Setup GPIO */
-    InitializeIO();
-
+    initializeIO();
 
     /* Create sd card task */
     _task_create(0,SDCARD_TASK,0);
