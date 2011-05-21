13e2006ea5466f66274ec5050882ee259a024355
T.J. Telan <t.telan@gmail.com>
2011-05-21 03:28:34 -0700

    Added led control prototypes. Also #include'ed homewatchdog.h

--- web/cgi.h
+++ web/cgi.h
@@ -7,6 +7,8 @@
 #ifndef _cgi_h_
 #define _cgi_h_
 
+#include "homewatchdog.h"
+
 int cgi_rtc_data(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio1(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio2(HTTPD_SESSION_STRUCT *session);
@@ -17,6 +19,10 @@ int cgi_toggle_gpio6(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio7(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio8(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio9(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_led1(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_led2(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_led3(HTTPD_SESSION_STRUCT *session);
+int cgi_toggle_led4(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_all(HTTPD_SESSION_STRUCT *session);
 int cgi_allOn(HTTPD_SESSION_STRUCT *session);
 int cgi_allOff(HTTPD_SESSION_STRUCT *session);
