72c1c0e88c0c333cc120c85d283fbe15aa0218f5
T.J. Telan <t.telan@gmail.com>
2011-05-20 19:57:15 -0700

    Fixing broken build. Adding missing proto and macro from Freescale's code.

--- web/cgi.h
+++ web/cgi.h
@@ -7,6 +7,7 @@
 #ifndef _cgi_h_
 #define _cgi_h_
 
+int cgi_rtc_data(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio1(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio2(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_gpio3(HTTPD_SESSION_STRUCT *session);
@@ -19,5 +20,13 @@ int cgi_toggle_gpio9(HTTPD_SESSION_STRUCT *session);
 int cgi_toggle_all(HTTPD_SESSION_STRUCT *session);
 int cgi_allOn(HTTPD_SESSION_STRUCT *session);
 int cgi_allOff(HTTPD_SESSION_STRUCT *session);
+
+#define CGI_SEND_NUM(val)                   \
+{                                           \
+        char str[20], *pstr;                \
+        pstr = &str[0];                     \
+        sprintf(pstr, "%ld\n", val);        \
+        httpd_sendstr(session->sock, str);  \
+}
 
 #endif
