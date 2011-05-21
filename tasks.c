1aecea77a2210d6e8b065a027b5ea37f376464e0
T.J. Telan <t.telan@gmail.com>
2011-05-20 19:52:35 -0700

    Formatting TASK_TEMPLATE_STRUCT

--- tasks.c
+++ tasks.c
@@ -14,26 +14,18 @@
 #error This application requires BSPCFG_ENABLE_IO_SUBSYSTEM defined non-zero in user_config.h. Please recompile BSP with this option.
 #endif
 
-
 #ifndef BSP_DEFAULT_IO_CHANNEL_DEFINED
 #error This application requires BSP_DEFAULT_IO_CHANNEL to be not NULL. Please set corresponding BSPCFG_ENABLE_TTYF to non-zero in user_config.h and recompile BSP with this option.
 #endif
 
-
-/* Task IDs */
-
-
-
 const TASK_TEMPLATE_STRUCT  MQX_template_list[] = 
 { 
-   /* Task Index,   Function,   Stack,  Priority, Name,     Attributes,          Param, Time Slice */
-    { START_TASK,   start_task, 1500,   5,        "start",  MQX_AUTO_START_TASK, 0,     0 },
-    { SDCARD_TASK,   sdcard_task,      1500,    11,      "SDcard", 0, 0,     0 },
-    { SHELL_TASK,    shell_task,       1500,    14,      "Shell",  0, 0,     0 },
+   /* Task Index,   Function,       Stack,  Priority,   Name,     Attributes,           Param, Time Slice */
+    { START_TASK,   start_task,     1500,   5,          "start",  MQX_AUTO_START_TASK,  0,     0 },
+    { SDCARD_TASK,  sdcard_task,    1500,   11,         "SDcard", 0,                    0,     0 },
+    { SHELL_TASK,   shell_task,     1500,   14,         "Shell",  0,                    0,     0 },
 
     { 0 }
 };
-
-
 
 /* EOF */
