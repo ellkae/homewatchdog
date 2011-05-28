8b1850b440bf7a66d2505529f3cd2c900213aaa0
T.J. Telan <t.telan@gmail.com>
2011-05-27 20:34:35 -0700

    Testing some javascript trickery to hopefully fix race conditions between RTC module and GPIO control

--- web/webroot/js/gpio.js
+++ web/webroot/js/gpio.js
@@ -1,66 +1,66 @@
 <script language="javascript" src="request.js"></script>
 
 function toggle_led1() {
-            makeRequest("toggleled1.cgi");
+    makeRequest("toggleled1.cgi");
 }
 
 function toggle_led2() {
-            makeRequest("toggleled2.cgi");
+    makeRequest("toggleled2.cgi");
 }
 
 function toggle_led3() {
-            makeRequest("toggleled3.cgi");
+    makeRequest("toggleled3.cgi");
 }
 
 function toggle_led4() {
-            makeRequest("toggleled4.cgi");
+    makeRequest("toggleled4.cgi");
 }
 
 function toggle_gpio1() {
-            makeRequest("togglegpio1.cgi");
+    makeRequest("togglegpio1.cgi");
 }
 
 function toggle_gpio2() {
-            makeRequest("togglegpio2.cgi");
+    makeRequest("togglegpio2.cgi");
 }
 
 function toggle_gpio3() {
-            makeRequest("togglegpio3.cgi");
+    makeRequest("togglegpio3.cgi");
 }
 
 function toggle_gpio4() {
-            makeRequest("togglegpio4.cgi");
+    makeRequest("togglegpio4.cgi");
 }
 
 function toggle_gpio5() {
-            makeRequest("togglegpio5.cgi");
+    makeRequest("togglegpio5.cgi");
 }
 
 function toggle_gpio6() {
-            makeRequest("togglegpio6.cgi");
+    makeRequest("togglegpio6.cgi");
 }
 
 function toggle_gpio7() {
-            makeRequest("togglegpio7.cgi");
+    makeRequest("togglegpio7.cgi");
 }
 
 function toggle_gpio8() {
-            makeRequest("togglegpio8.cgi");
+    makeRequest("togglegpio8.cgi");
 }
 
 function toggle_gpio9() {
-            makeRequest("togglegpio9.cgi");
+    makeRequest("togglegpio9.cgi");
 }
 
 function invert() {
-            makeRequest("invert.cgi");
+    makeRequest("invert.cgi");
 }
 
 function allOn() {
-            makeRequest("allon.cgi");
+    makeRequest("allon.cgi");
 }
 
 function allOff() {
-            makeRequest("alloff.cgi");
+    makeRequest("alloff.cgi");
 }
 
--- web/webroot/js/rtc.js
+++ web/webroot/js/rtc.js
@@ -1,28 +1,106 @@
-<script language="javascript" src="request.js"></script>
+var currentHours=1;
+var currentMinutes=0;
+var currentSeconds=0;
 
 var data_value      = new Array(3);
-var data_received   = 0;
+var time_received   = 0;
 
-function parse_vars(data) {
-	var parsed = data.split( "\n" );
+function clock(){
 
-	data_value[0] 		= parsed[0];
-	data_value[1] 		= parsed[1];
-	data_value[2] 		= parsed[2];
+    if (currentSeconds < 59){
+        currentSeconds=currentSeconds+1;
+    }
+    else{
+        currentSeconds=0;
 
-	hour.innerHTML 	=   "<h1>"+data_value[0]+"</h1>";
-	minute.innerHTML 	= "<h1>"+data_value[1]+"</h1>";
-	second.innerHTML 	= "<h1>"+data_value[2]+"</h1>";
+        if (currentMinutes < 59){
+            currentMinutes=currentMinutes+1;
+        }
+        else{
+            currentMinutes=0;
 
-}
-function loop() {
-    if (!data_received){
-        makeRequest("rtcdata.cgi");
-//        makeRequest("updatestatus.cgi");
+            // 12 hour clock, for now until i figure out what rtc module uses
+            if (currentHours < 12){
+                currentHours=currentHours+1;
+            }
+            else{
+                currentHours=1;
+            }
+        }
     }
 
-    setTimeout("loop()", 1000);
+    // Update clock
+    hour.innerHTML      = currentHours;
+    minute.innerHTML    = currentMinutes;
+    second.innerHTML    = currentSeconds;
+
+    setTimeout("clock()", 1000);
+}
+
+
+function syncRequest(url) {
+    var http_request = false;
+
+    url = url + "?sid=" + Math.random();
+
+    time_received = 1;
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/xml');
+        }
+    }
+    else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+    }
+
+    http_request.onreadystatechange = function() { syncClockContents(http_request); };
+    http_request.open('GET', url, true);
+    http_request.send(null);
+}
+
+function syncClockContents(http_request) {
+    if (http_request.readyState == 4) {
+        if (http_request.status == 200) {
+            syncClock(http_request.responseText);
+            time_received = 0;
+        }
+        else {
+            time_received = 0;
+//          alert('There was a problem with the request.');
+//          alert( http_request.status );
+//
+        }
+    }
+}
+
+
+function syncClock(data) {
+    var parsed = data.split( "\n" );
+
+    data_value[0]   = parsed[0];
+    data_value[1]   = parsed[1];
+    data_value[2]   = parsed[2];
+
+    currentHours    = data_value[0];
+    currentMinutes  = data_value[1];
+    currentSeconds  = data_value[2];
 
 }
 
-window.onload=loop;
+
+window.onload=syncRequest("rtcdata.cgi");
+window.onload=clock;
